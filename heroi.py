5dcbd64dbb5b95b38937a54167a1b7e4f44ab1d7
pmarangon <56054345+pmarangon@users.noreply.github.com>
2024-01-11 22:19:43 -0300

    Create heroi.py
    Adiciona Classe Heroi

---
+++ heroi.py
@@ -0,0 +1,25 @@
+class Heroi:
+    def __init__(self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+        if self.tipo == "mago":
+            ataque = "usou magia"
+        elif self.tipo == "guerreiro":
+            ataque = "usou espada"
+        elif self.tipo == "monge":
+            ataque = "usou artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "usou shuriken"
+        else:
+            ataque = "usou um ataque indefinido"
+
+        mensagem = f"O {self.tipo} atacou usando {ataque}"
+        print(mensagem)
+
+
+
+heroi_mago.atacar()
+heroi_guerreiro.atacar()
